709bffe44fef98859e526649179c86af0751e0a2
Noelia Parra <65130588+Noelia-Parra@users.noreply.github.com>
2020-09-26 14:06:22 +0200

    Add files via upload

---
+++ weather-app/script.js
@@ -0,0 +1,24 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${formatHours(timestamp)}`;
+}
+
+function search(city) {
+  let apiKey = "efdb90508378d538c76c3382a5e5f2bb";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+
+  apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(dispalyForecast);
+}
